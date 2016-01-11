80336a9e64d4e2e2350da1562614778a68367dd1
mgrcar <miha.grcar@ijs.si>
2016-01-11 22:01:39 +0100

    Fixed compile script

--- Compile.ps1
+++ Compile.ps1
@@ -4,9 +4,9 @@ $scriptRoot = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
 $files = (Get-ChildItem "$scriptRoot" -Filter *.java -Recurse).FullName
 
-if (Test-Path "$scriptRoot\bin") { rm "$scriptRoot\bin" -Recurse -Force }
-mkdir "$scriptRoot\bin" | Out-Null
+if (Test-Path "$scriptRoot\lib") { rm "$scriptRoot\lib" -Recurse -Force }
+mkdir "$scriptRoot\lib" | Out-Null
 
-javac -d "$scriptRoot\bin" $files
+javac -d "$scriptRoot\lib" $files
 
-jar cf "$scriptRoot\bin\Snowball.jar" "$scriptRoot\bin"
+jar cf "$scriptRoot\lib\Snowball.jar" "$scriptRoot\lib"

42f551a06f2b7838c4e726742c99354bdede61a1
mgrcar <miha.grcar@ijs.si>
2016-02-03 11:01:37 +0100

    Implemented dataset reader

--- latino4j-core/src/test/java/org/latinolib/model/LinearSvmModelTest.java
+++ latino4j-core/src/test/java/org/latinolib/model/LinearSvmModelTest.java
@@ -2,17 +2,69 @@ package org.latinolib.model;
 
 import de.bwaldvogel.liblinear.Parameter;
 import de.bwaldvogel.liblinear.SolverType;
+
 import org.junit.Test;
 import org.latinolib.SparseVector;
-import org.latinolib.model.*;
+import org.latinolib.VectorEntry;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.text.NumberFormat;
+import java.text.ParseException;
+import java.util.Locale;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static org.junit.Assert.*;
 
 /**
- * Author saxo
+ * Author saxo, mIHA
  */
 public class LinearSvmModelTest
 {
+    private static LabeledDataset<Double, SparseVector> readDataset(BufferedReader reader) throws IOException, ParseException {
+        String line;
+        LabeledDataset<Double, SparseVector> ds = new LabeledDataset<Double, SparseVector>();
+        while ((line = reader.readLine()) != null) {
+            SparseVector vec = new SparseVector();
+            Pattern labelPattern = Pattern.compile("^(?<label>[+-]?\\d+([.]\\d+)?)(\\s|$)");
+            Pattern featurePattern = Pattern.compile("(?i)(?<feature>(\\d+|qid)):(?<weight>[-]?[\\d\\.]+)");
+            if (!line.startsWith("#")) {
+                Matcher labelMatch = labelPattern.matcher(line);
+                labelMatch.find();
+                double label = Double.parseDouble(labelMatch.group("label"));
+                Matcher match = featurePattern.matcher(line);
+                while (match.find()) {
+                    int feature = Integer.parseInt(match.group("feature"));
+                    double weight = NumberFormat.getInstance(Locale.US).parse(match.group("weight")).doubleValue();
+                    vec.add(feature, weight);
+                }
+                ds.add(label, vec);
+            }
+        }
+        return ds;
+    }
+
+    @Test
+    public void testInduction() throws IOException, ParseException {
+        InputStream is = LinearSvmModelTest.class.getResourceAsStream("inductive/train.dat");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        LabeledDataset<Double, SparseVector> ds = readDataset(reader);
+        // debug code
+        for (LabeledExampleEntry<Double, SparseVector> le : ds) {
+            System.out.print(le.getLabel() + " ");
+            for (VectorEntry item : le.getExample()) {
+                System.out.print(item.getIndex() + ":" + item.getData() + " ");
+            }
+            System.out.println();
+        }
+        // end of debug code
+        // ...
+        reader.close();
+    }
+
     @Test
     public void testTrain() throws Exception {
 
